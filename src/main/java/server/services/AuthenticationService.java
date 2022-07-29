04200836869195d65a2ef2e494b193d673578fac
Alexey Shalygin <84471084+KillKennyXXX@users.noreply.github.com>
2022-07-30 00:32:20 +0300

    Initial commit

---
+++ src/main/java/server/services/AuthenticationService.java
@@ -0,0 +1,6 @@
+package server.services;
+
+public interface AuthenticationService {
+    String getUsernameByLoginAndPassword(String login, String password);
+    String getUserList();
+}
---
+++ src/main/java/server/services/impl/SqlAuthenticationServiceImpl.java
@@ -0,0 +1,15 @@
+package server.services.impl;
+
+import server.services.AuthenticationService;
+
+public class SqlAuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public String getUsernameByLoginAndPassword(String login, String password) {
+        return null;
+    }
+
+    @Override
+    public String getUserList() {
+        return null;
+    }
+}
